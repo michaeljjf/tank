5ef21a74e0f0b6ce733655b1e1af8fbe56049fac
michaeljjf <michaeljjf@126.com>
2022-02-07 22:05:20 +0800

    定时刷新坐标

--- src/main/java/com/jiangjf/Main.java
+++ src/main/java/com/jiangjf/Main.java
@@ -7,7 +7,11 @@ package com.jiangjf;
  * @date 2022/2/7
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
+        while (true) {
+            Thread.sleep(50);
+            tankFrame.repaint();
+        }
     }
 }
--- src/main/java/com/jiangjf/TankFrame.java
+++ src/main/java/com/jiangjf/TankFrame.java
@@ -1,6 +1,8 @@
 package com.jiangjf;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -18,22 +20,24 @@ public class TankFrame extends Frame {
         // 设置窗口大小
         int width = 800;
         int height = 600;
-        setSize(width, height);
+        this.setSize(width, height);
         // 窗口居中
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((dimension.width - width) / 2, (dimension.height - height) / 2);
+        this.setLocation((dimension.width - width) / 2, (dimension.height - height) / 2);
         // 禁用最大化
-        setResizable(false);
-        setTitle("tank war");
+        this.setResizable(false);
+        this.setTitle("tank war");
         // 显示窗口
-        setVisible(true);
+        this.setVisible(true);
         // 监听关闭事件
-        addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+        // 键盘监听
+        this.addKeyListener(new MyKeyListener());
     }
 
     @Override
@@ -43,5 +47,20 @@ public class TankFrame extends Frame {
         g.fillRect(x, y, 50, 50);
         x += 10;
         y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("keyPressed");
+//            x += 10;
+//            y += 10;
+//            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("keyReleased");
+        }
     }
 }
