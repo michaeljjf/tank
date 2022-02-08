8902dbc0fc37d26bf896ed0cda0862814853acfe
michaeljjf <michaeljjf@126.com>
2022-02-08 12:51:46 +0800

    双缓冲解决画面闪的问题

--- src/main/java/com/jiangjf/TankFrame.java
+++ src/main/java/com/jiangjf/TankFrame.java
@@ -14,17 +14,16 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
 
+    private static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
     Tank myTank = new Tank(200, 200, Dir.DOWN);
     Bullet bullet = new Bullet(300, 300, Dir.DOWN);
 
     public TankFrame() {
         // 设置窗口大小
-        int width = 800;
-        int height = 600;
-        this.setSize(width, height);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         // 窗口居中
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation((dimension.width - width) / 2, (dimension.height - height) / 2);
+        this.setLocation((dimension.width - GAME_WIDTH) / 2, (dimension.height - GAME_HEIGHT) / 2);
         // 禁用最大化
         this.setResizable(false);
         this.setTitle("tank war");
@@ -46,6 +45,25 @@ public class TankFrame extends Frame {
         System.out.println("paint");
         myTank.paint(g);
         bullet.paint(g);
+    }
+
+    /**
+     * 双缓冲解决画面闪的问题
+     */
+    Image offScreenImage = null;
+
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics graphics = offScreenImage.getGraphics();
+        Color color = graphics.getColor();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        graphics.setColor(color);
+        paint(graphics);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     class MyKeyListener extends KeyAdapter {
