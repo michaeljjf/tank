ddd6a831167eb85ad8b02c2927d68b0da675878f
michaeljjf <michaeljjf@126.com>
2022-02-26 22:25:30 +0800

    增加注释

--- src/main/java/com/jiangjf/tank/GameModel.java
+++ src/main/java/com/jiangjf/tank/GameModel.java
@@ -37,7 +37,7 @@ public class GameModel {
         // 画出我方坦克
         myTank.paint(g);
 
-        // 画出敌方坦克（不能用foreach，因为迭代器会发生数组越界for不会）
+        // 画出敌方坦克、子弹、爆炸效果（不能用foreach，因为迭代器会发生数组越界for不会）
         for (int i = 0; i < objects.size(); i++) {
             objects.get(i).paint(g);
         }
